71919c1a4cd759690011c07d067d791dabd0ab2a
Stacy <hurrendor@gmail.com>
2018-12-15 00:46:35 +1300

    Add base AJAX example

---
+++ js/ajax.js
@@ -0,0 +1,12 @@
+/**Base Request Example**/
+var xhr = new XMLHttpRequest();
+
+xhr.onload = function();
+
+xhr.open('GET', 'data/data.html', true);
+
+xhr.send(null);
+
+/**Loading XML Example**/
+/**Loading HTML Example**/
+/**Loading JSON Example**/
