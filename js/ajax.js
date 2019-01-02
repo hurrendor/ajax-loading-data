fbc2dc41b662433ac2e55b4ac302899fbedc0648
Stacy <hurrendor@gmail.com>
2019-01-03 11:40:05 +1300

    Set up json parsing

--- js/ajax.js
+++ js/ajax.js
@@ -1,81 +1,89 @@
-// /****Base Request Example****/
-// var xhr = new XMLHttpRequest();
-// xhr.onload = function(
-//   if(xhr.status === 200) { //test for an OK from the server
-//     document.getElementById('content').innerHTML = xhr.responseText;
-//   }
-// );
-// xhr.open('GET', 'data/data.html', true);//set up the request
-// xhr.send(null);//send the request to the server
+var responseObject;
+
+function loadJSON() {
+  var jsonSetup = new XMLHttpRequest();
+  jsonSetup.onload = function() {
+    if(jsonSetup.status === 200) {;
+      responseObject = JSON.parse(jsonSetup.responseText);
+      // console.log(jsonSetup.responseText);
+      // responseObject = (jsonSetup.responseText[i]).toString();
+
+      var newContent = '';
+      // for (var i = 0; i < responseObject.country.length; i++) {
+      for ( i in responseObject.country) {
+        console.log(responseObject.country[i]);
+        newContent += '<article>';
+        for ( l in responseObject.country[i].location){
+          newContent += '<h3>' + responseObject.country[i].location[l].name + '</h3>';
+          newContent += '<p>' + responseObject.country[i].location[l].description + '</p>';
+        }
+        newContent += '</article>';
+      }
+
+      document.getElementById('main').innerHTML = newContent;
+    }
+  }
+  jsonSetup.open('GET', 'data/data.json', true);
+  jsonSetup.send(null);
+};
+
+function loadXML() {
+  var xmlSetup = new XMLHttpRequest();
+  xmlSetup.onload = function() {
+    if(xmlSetup.status === 200) {
+      console.log(xmlSetup.responseText);
+    }
+  }
+  xmlSetup.open('GET', 'data/data.xml', true);
+  xmlSetup.send(null);
+};
 
 
+//load data pertaining to that country
 
-
-
-/****Loading HTML Example****/
-
-var selectForm = document.getElementById('dropdown');
-
-function loadData() {
+function loadHTML() {
   var htmlSetup = new XMLHttpRequest();
-  var dataType = selectForm.value;
-  console.log(dataType);
+  // var jsonSetup = new XMLHttpRequest();
+  // var xmlSetup = new XMLHttpRequest();
+  var country = selectForm.value;
+
 
   htmlSetup.onload = function(){
     console.log('htmlsetup.onload structured');
     if(htmlSetup.status === 200) { //will not work locally, only on a server
+      // console.log(htmlSetup.responseText);
       document.getElementById('main').innerHTML = htmlSetup.responseText;
     }
-    console.log('initialized');
   };
 
-  // switch statement
-  switch(dataType) {
-    case 'html':
-    dataToLoad = 'data/data.html'
+//make an array and cycle thru data types?
+  htmlSetup.open('GET', 'data/data.html', true);
+  // htmlSetup.open('GET', data/data.xml, true);
+  htmlSetup.send(null);
+  loadJSON();
+};
+
+var selectForm = document.getElementById('dropdown');
+
+function loadData() {
+  var country = selectForm.value;
+  console.log(country);
+  switch(country) {
+    case 'kenya':
+    loadHTML();
+    console.log('kenya');
     break;
 
-    case 'json':
-    dataToLoad = 'data/data.json'
+    case 'italy':
+    loadJSON();
     break;
 
-    case 'xml':
-    dataToLoad = 'data/data.xml'
+    case 'china':
+    loadXML();
     break;
 
     default:
+    console.log('default called');
     break;
   }
-
-  console.log(dataToLoad);
-  htmlSetup.open('GET', dataToLoad, true);
-  htmlSetup.send(null);
-};
-
-  // console.log('htmlLoad init');
-
-/****Loading XML Example****/
-// var xmlLoad = new XMLHttpRequest();
-//
-// xmlLoad.onload = function(
-  //   if(xmlLoad.status === 200) {
-    //     document.getElementById('main').innerHTML = xmlLoad.responseText;
-    //   }
-    // );
-    //
-    // xmlLoad.open('GET', 'data/mapData.xml', true);
-    //
-    // xmlLoad.send(null);
-
-/****Loading JSON Example****/
-// var jsonLoad = new XMLHttpRequest();
-//
-// jsonLoad.onload = function(
-//   if(jsonLoad.status === 200) {
-//     document.getElementById('content').innerHTML = jsonLoad.responseText;
-//   }
-// );
-//
-// jsonLoad.open('GET', 'data/data.html', true);
-//
-// jsonLoad.send(null);
+}
